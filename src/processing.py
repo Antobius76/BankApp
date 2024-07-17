8e6a02970da09ad35deffc721b1d76d7920e1e1c
Dmitry <dmitry_antonov@inbox.ru>
2024-07-17 13:12:37 +0400

    processing corrections

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,15 @@
-def filter_by_state(lst: list, state="EXECUTED") -> list | None:
+def filter_by_state(lst: list[dict], state="EXECUTED") -> list[dict]:
     """Функция прнимает список словарей и возвращает новый
     отфильтрованный по значению state список"""
-
     new_lst = []
     for dct in lst:
         if dct.get("state") == state:
-            new_lst.append(dict)
+            new_lst.append(dct)
     return new_lst
 
 
-def sort_by_date(dict_list: list, reverse: bool = True) -> list:
+def sort_by_date(dct_list: list[dict], reverse: bool = True) -> list[dict]:
     """Функция прнимает список словарей и
     возвращает отсортированный по дате новый список"""
-    sorted_dict_list = sorted(dict_list, key=lambda dct: dct.get("date"), reverse=reverse)
-
-    return sorted_dict_list
+    lst.sort(key=lambda x: x["date"])
+    return lst
